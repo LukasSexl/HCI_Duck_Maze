9e0ea47d9857bd561311ff79053bbefef5521541
LukasSexl <lukassexl02@gmail.com>
2025-06-12 16:15:25 +0200

    added logic to plaur movement

--- character.gd
+++ character.gd
@@ -1,8 +1,29 @@
 extends CharacterBody2D
+const speed = 400
 
 func _physics_process(delta):
+	var ardVal = 0
 	var csharp_node = get_node("..")
-	velocity.x = int(csharp_node.serialMessage);
-
+	#velocity.x = int(csharp_node.serialMessage);
+	ardVal = int(csharp_node.serialMessage);
+	
+	if(ardVal == 5): #right
+		velocity.x = speed
+		velocity.y = 0
+	elif(ardVal == 7): #left
+		velocity.x = -speed
+		velocity.y = 0
+	elif(ardVal == 8): #up
+		velocity.x = 0
+		velocity.y = -speed
+	elif(ardVal == 9): #down
+		velocity.x = 0
+		velocity.y = speed
+	else:
+		#player_animation(0)
+		velocity.x = 0
+		velocity.y = 0
+		
 	#print(csharp_node.serialMessage);
+	
 	move_and_slide()
