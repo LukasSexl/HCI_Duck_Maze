5a397c5ee7e6253abf0b4dd7c42d2c7b2bd8673f
LukasSexl <lukassexl02@gmail.com>
2025-06-20 16:17:22 +0200

    final

--- maze.gd
+++ maze.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var tilemap = $TileMapLayer
 
 # Constants
-const ROWS = 5
-const COLS = 5
+const ROWS = 11
+const COLS = 11
 const WALL = Vector2i(0, 0)  # Wall tile coordinates in tileset
 const PATH = Vector2i(1, 0)  # Path tile coordinates in tileset
 
